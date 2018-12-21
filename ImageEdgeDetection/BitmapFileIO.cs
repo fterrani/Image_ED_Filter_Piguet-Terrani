297fefe6e7c9dfca66fb2d3bff700fd5e07f63ce
PiguetNico <piguetnico@gmail.com>
2018-12-21 22:12:46 +0100

    Creating all interfaces and a class
    Created :
    - IBitmapFileIO & BitmapFileIO
    - IBitmapEditor
    - IBitmapViewer
    
    No modification yet on Mainform.cs code !

---
+++ ImageEdgeDetection/BitmapFileIO.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageEdgeDetection
+{
+    class BitmapFileIO : IBitmapFileIO
+    {
+        private Bitmap originalBitmap = null; // Original image (original size)
+        private StreamReader streamReader;
+        private StreamWriter streamWriter;
+        private ImageFormat imgFormat;
+
+        public Bitmap ReadBitmap( string file )
+        {           
+            // Loading selected image in originalBitmap
+            streamReader = new StreamReader(file);
+            originalBitmap = (Bitmap)Image.FromStream(streamReader.BaseStream);
+            streamReader.Close();                
+            return originalBitmap;
+        }
+
+        public bool WriteBitmap( Bitmap bitmap, string file )
+        {         
+            string fileExtension = Path.GetExtension(file).ToUpper();
+            imgFormat = ImageFormat.Png;
+
+            if (fileExtension == "BMP")
+            {
+                imgFormat = ImageFormat.Bmp;
+            }
+            else if (fileExtension == "JPG")
+            {
+                imgFormat = ImageFormat.Jpeg;
+            }
+
+            // Saving the result image in a file
+            streamWriter = new StreamWriter(file, false);
+            try
+            {
+                bitmap.Save(streamWriter.BaseStream, imgFormat);
+                streamWriter.Flush();
+                streamWriter.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }            
+        }
+    }
+}
---
+++ ImageEdgeDetection/IBitmapFileIO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ImageEdgeDetection
+{
+    interface IBitmapFileIO
+    {
+        // Return a bitmap from a given path
+        Bitmap ReadBitmap( string file );
+
+        // Return true if the bitmap is successfully saved
+        bool WriteBitmap( Bitmap bitmap, string file);
+    }
+}
